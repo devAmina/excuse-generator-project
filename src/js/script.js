0604716385ad183e9d6bcef2e4394f3b14b5bc74
devAmina <59917537+devAmina@users.noreply.github.com>
2024-12-04 07:50:45 +0000

    excuseTextCommit

--- src/js/script.js
+++ src/js/script.js
@@ -8,8 +8,41 @@ window.onload = function () {
 
     // 2. Generate a random excuse
 
-    const excuseText = `The dog ate my homework for breakfast`;
-
+    const arrayExecuteText = ['The dog ate my homework for breakfast',
+        'A dragon stole my sword last night',
+        'The intern misplaced the report before the meeting',
+        'I accidentally forgot your birthday last weekend',
+        'Spider-Man webbed my laptop last night',
+        'The AI chatbot rewrote my code this morning'];
+    const arrayExecuteTextSubject = ['The dog ',
+        'A dragon ',
+        'The intern ',
+        'I ',
+        'Spider-Man ',
+        'The AI chatbot '];
+    const arrayExecuteTextVerb = ['ate ',
+        'stole ',
+        'misplaced ',
+        'accidentally forgot ',
+        'webbed ',
+        'rewrote '];
+    const arrayExecuteTextObject = ['my homework ',
+        'my sword ',
+        'the report ',
+        'your birthday ',
+        'my laptop ',
+        'my code '];
+    const arrayExecuteTextTime = ['for breakfast',
+        'last night.',
+        'before the meeting.',
+        'last weekend.',
+        'last night.',
+        'this morning.'];
+    const excuseTextIndex = Math.floor(Math.random() * arrayExecuteText.length);
+   
     // 3. Inseart the excuse into the excuse element
-    excuse.innerHTML = excuseText;
+    excuse.innerHTML = arrayExecuteTextSubject[excuseTextIndex] + 
+                       arrayExecuteTextVerb[excuseTextIndex] + 
+                       arrayExecuteTextObject[excuseTextIndex] + 
+                       arrayExecuteTextTime[excuseTextIndex];
 }
